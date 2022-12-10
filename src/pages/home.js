48ac63d2b283a9ce9138d99424d1127d432374e7
ram <khaled_17@agr.sohag.edu.eg>
2022-12-10 16:19:26 +0200

    home fex

--- src/pages/home.js
+++ src/pages/home.js
@@ -20,7 +20,7 @@ const Home = () => {
             {/* <Monthspecials /> */}
             <Filter />
             <Carousels />
-            <Carou/>
+            {/* <Carou/> */}
 
 
             <Banners />
