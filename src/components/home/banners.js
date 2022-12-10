fcfcad5d61cc639048afeb3e7ff5db8109284a04
ram <khaled_17@agr.sohag.edu.eg>
2022-12-10 16:30:07 +0200

    Banners is done css

--- src/components/home/banners.js
+++ src/components/home/banners.js
@@ -35,28 +35,15 @@ const Banners = () => {
       <div className="row">
 
 
-        <div className="col ">
-
-
-
-
+        <div className=" px-0 col  ">
+          <img className='img-fluid w-100 pb-2' src="https://elabd.blob.core.windows.net/elabdcontainer/7a27c3da-af7e-4c8c-a1a2-4c12821e97a9-638011841777956594.jpg" alt="" />
           <img className='img-fluid w-100' src="https://elabd.blob.core.windows.net/elabdcontainer/7a27c3da-af7e-4c8c-a1a2-4c12821e97a9-638011841777956594.jpg" alt="" />
-
-
-          <img className='img-fluid w-100' src="https://elabd.blob.core.windows.net/elabdcontainer/7a27c3da-af7e-4c8c-a1a2-4c12821e97a9-638011841777956594.jpg" alt="" />
-
-
-
         </div>
-
-        <div className="col-sm-12  col-md-5">
-          <img className='img-fluid' src="https://elabd.blob.core.windows.net/elabdcontainer/759c3ac1-3ee5-48fe-a02f-80180b50de06-638016987660434646.jpg" alt="" />
-
+        <div className="col-sm-12  col-md-4 ">
+          <img style={{heigdht:'500px'}} className=' h-100 img-fluid' src="https://elabd.blob.core.windows.net/elabdcontainer/759c3ac1-3ee5-48fe-a02f-80180b50de06-638016987660434646.jpg" alt="" />
         </div>
-
-
-        <div className="col">
-          <img className='img-fluid w-100' src="https://elabd.blob.core.windows.net/elabdcontainer/7a27c3da-af7e-4c8c-a1a2-4c12821e97a9-638011841777956594.jpg" alt="" />
+        <div className="px-0 col ">
+          <img className='img-fluid w-100 pb-2' src="https://elabd.blob.core.windows.net/elabdcontainer/7a27c3da-af7e-4c8c-a1a2-4c12821e97a9-638011841777956594.jpg" alt="" />
           <img className='img-fluid w-100' src="https://elabd.blob.core.windows.net/elabdcontainer/7a27c3da-af7e-4c8c-a1a2-4c12821e97a9-638011841777956594.jpg" alt="" />
         </div>
 
