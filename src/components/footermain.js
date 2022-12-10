21e4f9533b0c29a37b8431d4156d02e607017e89
ram <khaled_17@agr.sohag.edu.eg>
2022-12-10 21:40:33 +0200

    test

--- src/components/footermain.js
+++ src/components/footermain.js
@@ -95,7 +95,7 @@ const Footermain = () => {
                         <li>Stores</li>
                         <li><Link style={font} to="/TermsandConditions">Terms and Conditions</Link></li>
                     </ul>
-
+{/* و */}
                 </div>
 
 
