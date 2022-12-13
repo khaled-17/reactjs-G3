f8f1e03993892d4723ec602d515e51a17d50fe9b
ram <khaled_17@agr.sohag.edu.eg>
2022-12-14 01:39:13 +0200

    un marge

--- src/App.js
+++ src/App.js
@@ -12,12 +12,6 @@ import TermsandConditions from './pages/TermsandConditions';
 import Singleproduct from './pages/singleproduct';
 import AboutUs from './pages/AboutUs';
 
-// Mahmoud's pages import
-import Zone from './pages/zone/Zone-page'
-import Login from './pages/login/Login'
-import Registration from './pages/Registration/Registration'
-import Cart from './pages/cart/Cart'
-
 
 function App() {
   const { t, i18n } = useTranslation();
@@ -35,19 +29,12 @@ function App() {
         <div >
 
           <Routes>
-          <Route path="/" element={<Zone />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/Registration" element={<Registration />} />
-          <Route path="/Cart" element={<Cart />} />
-
-
-
-            <Route path="/Home" element={<Home />} />
-            <Route path="/*" element={<NotFound />} />
-            <Route path="/CategoryID" element={<CategoryID />} />
-            <Route path="/TermsandConditions" element={<TermsandConditions />} />
-            <Route path="/AboutUs" element={<AboutUs />} />
-            <Route path="/Singleproduct" element={<Singleproduct />} />
+            <Route path="/" element={<Home/>} />
+            <Route path="/*" element={<NotFound/>} />
+            <Route path="/CategoryID" element={<CategoryID/>} />
+            <Route path="/TermsandConditions" element={<TermsandConditions/>} />
+            <Route path="/AboutUs" element={<AboutUs/>} />
+            <Route path="/Singleproduct" element={<Singleproduct/>} />
           </Routes>
         </div>
 
