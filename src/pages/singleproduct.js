1c1b4f751eec954ccacc87c141f6de2b35f39c97
ram <khaled_17@agr.sohag.edu.eg>
2022-12-17 08:35:49 +0200

    fixx sp page

--- src/pages/singleproduct.js
+++ src/pages/singleproduct.js
@@ -3,6 +3,11 @@ import Rating from './../components/Rating';
 import Footermain from './../components/footer/footermain';
 import Carousels from './../components/Carousel';
 
+
+import AddReview from './../components/addReview';
+
+
+
 import Footerc from './../components/footer/footerc';
 
 import { Button } from 'react-bootstrap';
@@ -29,7 +34,8 @@ const Singleproduct = () => {
 
 
     }
-
+    // console.log(window.location.href);
+    const curntURL = window.location.href
     return (
         <>
             <Header />
@@ -55,7 +61,37 @@ const Singleproduct = () => {
                         <div className="d-flex flex-row">
                             <Rating />
                             <span className=" mx-2">|</span>
-                            <button class=" "  >+ Add Review</button>
+                            <AddReview />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                         </div>
                         <div className="d-flex flex-row">
                             <h3>45 EGP</h3>
@@ -77,16 +113,36 @@ const Singleproduct = () => {
                         </div>
 
                         <div class="alert m-0 alert-secondary d-flex justify-content-around p-0" style={alerth} role="alert">
+                           
                             <p class=""   >
                                 <span class="">
                                     <span class="bg-secondary  rounded-5  text-white " style={icon}>
                                         <FaShuttleVan />
                                     </span >
                                     <span class="fw-semibold fs-6"> Delivered Within :</span>
-                                </span> Delivered Within</p>
+                                </span> Delivered Within
+                                
+                            </p>
+
+                           <div>
+                           <a class="m-3 fb-xfbml-parse-ignore text-secondary" target="_blank" href={"https://www.facebook.com/sharer/sharer.php?u=https://YOUR-SITE-HERE.com" + curntURL}><svg xmlns="http://www.w3.org/2000/svg"  height="26" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                            </svg></a>
+                            <a class="twitter-share-button text-secondary" target="_blank" href={"https://twitter.com/intent/tweet?url=" + curntURL + "&hashtags=elabdfoods"}><svg xmlns="http://www.w3.org/2000/svg"  height="26" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                            </svg></a>
+                           </div>
+                            
 
 
+                        <div>
                             <p class="ms-auto"><AiOutlineHeart />Add To Wish list</p>
+                        </div>
+
+
+                        
+                        
+                        
                         </div>
 
 
@@ -122,10 +178,10 @@ const Singleproduct = () => {
             </div>
             <h2 className='m-4 col-12 d-flex justify-content-center'>You may be interested in</h2>
 
-            <Carousels/>
+            <Carousels />
             <h2 className='m-4 col-12 d-flex justify-content-center'>Who Viewed This Viewed That</h2>
 
-            <Carousels/>
+            <Carousels />
 
             <Footermain />
             <Footerc />
