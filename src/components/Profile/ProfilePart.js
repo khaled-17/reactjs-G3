613240ac75715ca279ee8b0a9eeb6c6b6f5eb7db
ram <khaled_17@agr.sohag.edu.eg>
2022-12-18 14:24:54 +0200

    Merge branch 'master' into ff

---
+++ src/components/Profile/ProfilePart.js
@@ -0,0 +1,109 @@
+import React from 'react';
+
+const ProfilePart = () => {
+    return (
+        <>
+
+            <form class="row g-3">
+
+
+                <div class=" col-md-6">
+                    <label for="validationDefault01" class="form-label">First name</label>
+                    <input type="text" class="form-control" id="validationDefault01" value="Mark" required />
+                </div>
+
+                <div class=" col-md-6">
+                    <label for="validationDefault02" class="form-label">Last name</label>
+                    <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                </div>
+
+                <div class=" col-md-6">
+                    <label for="floatingInputValue">email</label>
+                    <input type="email" class="form-control" id="floatingInputValue" placeholder="name@example.com" value="test@example.com" />
+                </div>
+
+                <div class=" col-md-6">
+                    <label for="floatingPassword">Password</label>
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                </div>
+                <div class=" col-md-6">
+                    <label for="floatingMobile">Mobile</label>
+                    <input type="number" class="form-control" id="floatingMobile" placeholder="Mobile" />
+                </div>
+
+
+
+
+                <div class=" col-md-6">
+                    <label for="validationDefault04" class="form-label">Zone</label>
+                    <select class="form-select" id="validationDefault04" required>
+                        <option selected disabled value="">Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+
+
+
+
+                <div class=" col-md-6 d-flex">
+
+                    <div class=" col m-1">
+                        <label for="validationDefault05" class="form-label">Day</label>
+                        <input type="number" class="form-control" id="validationDefault05" required />
+                    </div>
+                    <div class=" col m-1">
+                        <label for="validationDefault05" class="form-label">month</label>
+                        <input type="number" class="form-control" id="validationDefault05" required />
+                    </div>
+                    <div class=" col m-1">
+                        <label for="validationDefault05" class="form-label">year</label>
+                        <input type="number" class="form-control" id="validationDefault05" required />
+                    </div>
+
+
+
+                </div>
+
+                <div class=" col-md-6 d-flex">
+                </div>
+
+                <div class=" col-md-6 d-flex">
+
+                         <p className='mx-2'>Gender</p>
+ 
+
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="mail" value="option1" />
+                            <label class="form-check-label" for="mail">mail</label>
+                        </div>
+
+
+
+
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="fmail" value="option2" />
+                            <label class="form-check-label" for="fmail">fmail</label>
+                        </div>
+
+
+
+                    </div>
+                    <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+    <label class="form-check-label" for="exampleCheck1">subScribe to Out Newsletter</label>
+  </div>
+
+
+                    <div class=" col-12">
+                        <button class="btn btn-primary" type="submit">Submit form</button>
+                    </div>
+            </form>
+
+
+
+
+        </>
+    );
+}
+
+export default ProfilePart;
