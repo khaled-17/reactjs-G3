e992ddd052abac064dd6f4b3825d5ca532f0a6f2
ram <khaled_17@agr.sohag.edu.eg>
2022-12-23 19:30:10 +0200

    Update Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,47 +1,47 @@
-import React from 'react'
-import Carousel from 'react-grid-carousel'
-import Cards from './cards';
+// import React from 'react'
+// import Carousel from 'react-grid-carousel'
+// import Cards from './cards';
 
-const Filter = () => {
-  return (
-    <Carousel cols={4} rows={1} gap={0} loop={false}>
-      <h1>Filter</h1>
-      <Carousel.Item>
+// const Filter = () => {
+//   return (
+//     <Carousel cols={4} rows={1} gap={0} loop={false}>
+//       <h1>Filter</h1>
+//       <Carousel.Item>
          
           
-        <Cards/>
+//         <Cards/>
  
-      </Carousel.Item>
-      <Carousel.Item>
+//       </Carousel.Item>
+//       <Carousel.Item>
          
           
-        <Cards/>
+//         <Cards/>
  
-      </Carousel.Item>
-      <Carousel.Item>
+//       </Carousel.Item>
+//       <Carousel.Item>
          
           
-        <Cards/>
+//         <Cards/>
  
-      </Carousel.Item>
-      <Carousel.Item>
+//       </Carousel.Item>
+//       <Carousel.Item>
 
 
 
-        <Cards />
-      </Carousel.Item>
-      <Carousel.Item>
+//         <Cards />
+//       </Carousel.Item>
+//       <Carousel.Item>
 
 
 
-        <Cards />
-      </Carousel.Item>
+//         <Cards />
+//       </Carousel.Item>
 
       
       
 
-      {/* ... */}
-    </Carousel>
-  )
-}
-export default Filter;
+//       {/* ... */}
+//     </Carousel>
+//   )
+// }
+// export default Filter;
